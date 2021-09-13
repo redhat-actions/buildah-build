7c11f3b4fb98108cc555389d0ab8de59ad34b503
divyansh42 <diagrawa@redhat.com>
2021-09-13 17:32:16 +0530

    Add script in a file to install latest buildah
    Signed-off-by: divyansh42 <diagrawa@redhat.com>

---
+++ .github/install_latest_buildah.sh
@@ -0,0 +1,7 @@
+# https://github.com/containers/buildah/blob/main/install.md
+. /etc/os-release
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${ID^}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${ID^}_${VERSION_ID}/Release.key -O Release.key
+sudo apt-key add - < Release.key
+sudo apt-get update -qq
+sudo apt-get -qq -y install buildah
